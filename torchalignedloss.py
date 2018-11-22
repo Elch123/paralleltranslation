b42bd02dda3829de5b7dc256dcead9f4f37421b0
Elchanan Haas <haas2376@gmail.com>
2018-11-22 14:21:03 +0200

    handle blanks

--- torchalignedloss.py
+++ torchalignedloss.py
@@ -5,28 +5,39 @@ import makeendeprocessors
 from makeendeprocessors import decode
 from makeendeprocessors import decodearray
 import torch
-from torchmodel import Cnn
+from torchmodel import Cnn,ResNet
 from makebatches import Batch_maker
+import atexit
+import json
 (enprocessor,deprocessor)=makeendeprocessors.load()
 #hyperparameters
 params={
 'symbols_in_batch':1000,
-'num_hidden':400,
+'num_hidden':600,
 'epochs':100,
 'symbols':4000,
 'net_verbose':False,
+'batchnorm':False,
 }
+indel_penalty=-.05
 smoothing=.5
 blank_weight=.00
 scoreavg=0
+validlosses=[]
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
 traindata=Batch_maker("traindeen.pickle")
 validdata=Batch_maker("traindeen.pickle")
+def on_exit():
+    with open("validhistory",'w') as f:
+        json.dump(validlosses,f)
+atexit.register(on_exit)
 def alignment_score(label,tensor):
+    if(label==0):
+        return -tensor[label]*indel_penalty #don't reward extra blank labels more than the indel indel_penalty, to cancel that out
+        #This will prevent the creation of many blamks
     return tensor[label]
 def align(output,label): # This uses the Needleman Wunsch optimal alignment algrithm to align the outputs of the model with the labels
-    indel_penalty=-.05
     l_label=len(label)+1
     l_output=len(output)+1
     labelcopy=np.zeros(shape=(len(output)))
@@ -56,25 +67,18 @@ def align(output,label): # This uses the Needleman Wunsch optimal alignment algr
     k=l_label-1
     m=l_output-1
     path=[]
-    #print(array)
-    #print(trace)
-    #print(trace.shape)
     while(k>=0 and m>=0):
         #print(k,m)
         path.append((k,m))
         if(trace[k,m]==0): #Keep label
             k-=1
             m-=1
-            labelcopy[m]=label[k]
-        #Use elif statements to only trigger one of the cases, not multiple ones before the path is updated.
+            labelcopy[m]=label[k] #Use elif statements to only trigger one of the cases, not multiple ones before the path is updated.
         elif(trace[k,m]==1): #Insert label
             k-=1
         elif(trace[k,m]==2): #Delete label
             m-=1
-    #print(label.asnumpy().astype(np.int32))
-    #print(labelcopy.asnumpy().astype(np.int32))
     path=path[::-1]
-    #print(path)
     return labelcopy
 def alignbatch(outputs,labels): # Run alignment algorithm on each tensor in the batch
     aligned=np.zeros_like(labels)
@@ -89,41 +93,45 @@ print(geneb)
 a=align(genea,geneb)
 print(a)
 print(exiting)"""
-#model defanition
-#net=RNNmodel(params)
-net=Cnn(params)
+net=ResNet(params)
 net.to(device)
-loss_fn = torch.nn.CrossEntropyLoss(reduction='mean')
-optimizer = torch.optim.SGD(net.parameters(), lr=1e-1, momentum=0.9)
+mask=torch.ones((4000,))
+mask[0]=.1
+loss_fn = torch.nn.CrossEntropyLoss(reduction='mean',weight=mask.to(device))
+optimizer = torch.optim.SGD(net.parameters(), lr=3e-2, momentum=0.9,nesterov=True,weight_decay=1e-5)
 smoothed_loss="null"
-
 def maketorchbatch(gen):
     batch=gen.makebatch(params['symbols_in_batch'])
-    #print(batch.shape)
     return torch.from_numpy(batch).long()
 def tonumpy(tensor):
     return tensor.detach().cpu().numpy()
 def printvalid():
-    batch=maketorchbatch(validdata).to(device)#.as_in_context(model_ctx)
+    batch=maketorchbatch(validdata).to(device)
     data=batch[1]
     labels=batch[0]
-    output=net(data).permute(0,2,1).detach().cpu().numpy() #get the output as a channels last numpy array.
-    output=np.argmax(output,axis=2) #argmax it to get the real words
-    print(output)
-    print(decodearray(enprocessor,output))
+    output=net(data)
+    cpuoutput=tonumpy(output.permute(0,2,1)) #get the output as a channels last numpy array.
+    cpulabels=tonumpy(labels)
+    alignedlabels=alignbatch(cpuoutput,cpulabels)
+    alignedlabels=torch.from_numpy(alignedlabels).long().to(device) #reconvert it to a torch tensor
+    loss=loss_fn(output,alignedlabels)
+    cpuoutput=np.argmax(cpuoutput,axis=2) #argmax it to get the real words
+    #print(cpuoutput)
+    print(decodearray(enprocessor,cpuoutput))
     print("\n")
     print(decodearray(enprocessor,labels.detach().cpu().numpy()))
+    print("validation loss "+str(loss.item())+"\n")
+    validlosses.append(loss.item())
 for e in range(params['epochs']):
     #This loss function produces the one already done if it is the best, or else the closest improvement
     count=0
     while(True):
-        #print(dir(trainer))
         batch=maketorchbatch(traindata).to(device)
         data=batch[1]
         labels=batch[0]
         output=net(data)
-        cpuoutput=output.permute(0,2,1).detach().cpu().numpy()#convert the tensor to have the channel dimantion last for the alignment stage
-        cpulabels=labels.detach().cpu().numpy()
+        cpuoutput=tonumpy(output.permute(0,2,1))#convert the tensor to have the channel dimantion last for the alignment stage
+        cpulabels=tonumpy(labels)
         alignedlabels=alignbatch(cpuoutput,cpulabels)
         alignedlabels=torch.from_numpy(alignedlabels).long().to(device) #reconvert it to a torch tensor
         loss=loss_fn(output,alignedlabels)
--- torchmodel.py
+++ torchmodel.py
@@ -1,6 +1,29 @@
 import torch
 import torch.nn as nn
 
+
+class ResidualBlock(nn.Module):
+    def __init__(self,params):
+        self.params=params
+        super().__init__()
+        self.conv1=torch.nn.Conv1d(params['num_hidden'],params['num_hidden'], 3,padding=1)
+        self.bn1=torch.nn.BatchNorm1d(params['num_hidden'])
+        self.conv2=torch.nn.Conv1d(params['num_hidden'],params['num_hidden'], 3,padding=1)
+        self.bn2=torch.nn.BatchNorm1d(params['num_hidden'])
+        self.relu=torch.nn.ReLU()
+
+    def forward(self,x):
+        residual=x
+        if(self.params['batchnorm']):
+            x=self.bn1(x)
+        x=self.relu(x)
+        x=self.conv1(x)
+        if(self.params['batchnorm']):
+            x=self.bn2(x)
+        x=self.relu(x)
+        x=self.conv2(x)
+        x+=residual
+        return x
 class Cnn(nn.Module):
     def __init__(self,params):
         super().__init__()
@@ -19,4 +42,22 @@ class Cnn(nn.Module):
         #print(embedded.shape)
         out=self.model(embedded)
         #print(out.shape)
+        return out
+
+class ResNet(nn.Module):
+    def __init__(self,params):
+        super().__init__()
+        self.embed=torch.nn.Embedding(4000,params['num_hidden'])
+        self.model = torch.nn.Sequential(
+        torch.nn.Conv1d(params['num_hidden'],params['num_hidden'], 5,padding=2),
+        ResidualBlock(params),
+        ResidualBlock(params),
+        ResidualBlock(params),
+        ResidualBlock(params),
+        torch.nn.ReLU(),
+        torch.nn.Conv1d(params['num_hidden'],4000, 5,padding=2))
+    def forward(self,x):
+        embedded=self.embed(x)
+        embedded=embedded.permute(0,2,1)
+        out=self.model(embedded)
         return out
